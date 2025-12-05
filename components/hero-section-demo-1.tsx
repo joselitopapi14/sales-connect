7d60f7f566a860c6ea26e86ba09cfd544c4c33e5
joselitopapi14 <enriquejo2002@gmail.com>
2025-12-04 19:10:06 -0500

    refactor: Remove unused Navbar component and associated image from HeroSectionOne

--- components/hero-section-demo-1.tsx
+++ components/hero-section-demo-1.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "motion/react";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function HeroSectionOne() {
@@ -68,31 +67,6 @@ export default function HeroSectionOne() {
           <Button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-slate-700 transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-slate-700 dark:text-white dark:hover:bg-gray-900">
             Aprende más
           </Button>
-        </motion.div>
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: 10,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 0.3,
-            delay: 1.2,
-          }}
-          className="relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
-        >
-          <div className="w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700">
-            <Image
-              src="https://assets.aceternity.com/pro/aceternity-landing.webp"
-              alt="Landing page preview"
-              className="aspect-video h-auto w-full object-cover"
-              height={1000}
-              width={1000}
-            />
-          </div>
         </motion.div>
       </div>
     </div>
